d54e3fcc01e6eec60986e3ae65dca9155b15c2c7
XioniX17 <44603173+XioniX17@users.noreply.github.com>
2026-03-21 09:10:21 +0800

    Fixed issue in the Birb VS Pig AudioLesson
    There was an issue with going back and forth from Scenes Midterm_MainMenu and Midterm_Gameplay and vice versa but I managed to fix it

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -5,6 +5,12 @@ public class SceneChanger : MonoBehaviour
 {
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName); 
+        /*This doesn't work because in loading the Asset Package,
+         * the build profile in this project doesn't match the project this asset package was in
+         So going from Midterm_MainMenu scene cannot got to Midterm_Gameplay and vice versa
+        
+         Nvm I fixed it by adjusting the Scene Profile*/
+
     }
 }
